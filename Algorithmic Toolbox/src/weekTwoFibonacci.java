b3b875fcb97a348b4b680e92ef23d1c4b7970820
MINHO CHOI <minhocalgary@gmail.com>
2020-06-24 11:03:41 -0600

    finish fibonacci

--- Algorithmic Toolbox/src/weekTwoFibonacci.java
+++ Algorithmic Toolbox/src/weekTwoFibonacci.java
@@ -1,11 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class weekTwoFibonacci {
-  private static long calc_fib(int n) {
-    if (n <= 1)
+  static long calc_fib(int n) {
+    
+	  /** naive way
+	  if (n <= 1)
       return n;
 
-    return calc_fib(n - 1) + calc_fib(n - 2);
+    return calc_fib(n - 1) + calc_fib(n - 2); */
+	  
+	 //using array
+	  if (n <= 1)
+	  return n;
+	  
+	  long[] list = new long[n];
+	  
+	  list[0] = 1;
+	  list[1] = 1;
+	  
+	  for(int i=2; i <n; i++)
+	  {
+		  list[i] = list[i-1] + list[i-2];  
+	  }
+	  
+	  return (long)list[n-1];
+	  
   }
 
   public static void main(String args[]) {
---
+++ Algorithmic Toolbox/src/weekTwoFibonacciTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class weekTwoFibonacciTest {
+
+	@Test
+	void test() {
+		
+		assertEquals(55, weekTwoFibonacci.calc_fib(10), "test");
+		
+	}
+	
+}
+
+
