0648aecbc1dbd9525477c00d146baff139960720
MINHO CHOI <minhocalgary@gmail.com>
2020-09-10 17:54:10 -0600

    finished graph w1-2

--- Algorithms on Graphs/src/ConnectedComponents.java
+++ Algorithms on Graphs/src/ConnectedComponents.java
@@ -1,28 +1,40 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class ConnectedComponents {
-    private static int numberOfComponents(ArrayList<Integer>[] adj, ArrayList<Integer> visited, int x) {
+
+    private static ArrayList<Integer> reach(ArrayList<Integer>[] adj, ArrayList<Integer> visited, int x) {
 
         boolean visit = false;
+        visited.add(x);
+
+        for(int  i=0; i < adj[x].size(); i++) {
+
+            visit = false;
+
+            if(visited.contains(adj[x].get(i))) {
+                visit = true;
+            }
+
+            if(!visit) {
+                reach(adj, visited,adj[x].get(i));
+                }
+            }
+        return visited;
+    }
+
+
+    private static int numberOfComponents(ArrayList<Integer>[] adj, ArrayList<Integer> visitedV, int x) {
 
         for(int  i=0; i < adj.length; i++) {
 
-            for(int t=0; t < adj[i].size(); t++) {
-
-                visited.add(adj[i].get(t));
-                visit = false;
-
-                if(visited.contains(adj[i].get(t))) {
-                    visit = true;
-                }
-
-                if(!visit) {
-                    numberOfComponents(adj, visited, x);
+                if(!visitedV.contains(i)) {
+                    visitedV = reach(adj, visitedV, i);
                     x++;
                 }
-            }
         }
+
         return x;
     }
 
@@ -43,7 +55,7 @@ public class ConnectedComponents {
             adj[y - 1].add(x - 1);
         }
 
-        int x = 1;
+        int x = 0;
         ArrayList<Integer> visited = new ArrayList<Integer>();
 
         if(m == 0) {
