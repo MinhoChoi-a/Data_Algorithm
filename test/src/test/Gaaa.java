83edba0e605dc45eb239ede45f75206a62341e6d
MINHO CHOI <minhocalgary@gmail.com>
2020-09-30 08:23:22 -0600

    delete test

--- test/src/test/Gaaa.java
+++ test/src/test/Gaaa.java
@@ -11,6 +11,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
+import javax.swing.*;
+
 public class Gaaa {
 
 	private JFrame frame;
@@ -44,44 +46,44 @@ public class Gaaa {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		frame.getContentPane().add(panel, BorderLayout.WEST);
+		panel.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnNewButton1 = new JButton();
-		GridBagConstraints gbc_btnNewButton1 = new GridBagConstraints();
-		gbc_btnNewButton1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton1.gridx = 0;
-		gbc_btnNewButton1.gridy = 0;
-		panel.add(btnNewButton1, gbc_btnNewButton1);
+		JLabel lblNewLabel = new JLabel("New label");
+		panel.add(lblNewLabel, BorderLayout.NORTH);
 		
-		JButton btnNewButton2 = new JButton();
-		GridBagConstraints gbc_btnNewButton2 = new GridBagConstraints();
-		gbc_btnNewButton2.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton2.gridx = 1;
-		gbc_btnNewButton2.gridy = 0;
-		panel.add(btnNewButton2, gbc_btnNewButton2);
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		JButton btnNewButton3 = new JButton();
-		GridBagConstraints gbc_btnNewButton3 = new GridBagConstraints();
-		gbc_btnNewButton3.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton3.gridx = 2;
-		gbc_btnNewButton3.gridy = 0;
-		panel.add(btnNewButton3, gbc_btnNewButton3);
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnNewButton4 = new JButton();
-		GridBagConstraints gbc_btnNewButton4 = new GridBagConstraints();
-		gbc_btnNewButton4.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton4.gridx = 3;
-		gbc_btnNewButton4.gridy = 0;
-		panel.add(btnNewButton4, gbc_btnNewButton4);		
+		JPanel panel_3 = new JPanel();
+		panel_1.add(panel_3, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel_3.add(lblNewLabel_1);
+		
+		JPanel panel_4 = new JPanel();
+		panel_1.add(panel_4, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel_4.add(lblNewLabel_2);
+		
+		for(int i=0; i < 100; i++) {
+			JButton btnNewButton = new JButton("N");
+			gamePanel.add(btnNewButton);			
+		}
 		
 	}
+	
+	 
+	
+	
 
 }
